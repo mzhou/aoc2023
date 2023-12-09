892236bd9997b339ef604cb807e8435de3b38828
Michael Zhou <mzhou@cse.unsw.edu.au>
2023-12-09 19:08:23 +0900

    day 9

--- aoc2023_09/src/main.rs
+++ aoc2023_09/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::stdin;
+
+fn differentiate(numbers: &[i64]) -> Vec<i64> {
+    let mut ret = Vec::<i64>::new();
+
+    for i in 1..numbers.len() {
+        ret.push(numbers[i] - numbers[i - 1]);
+    }
+
+    ret
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut total = 0;
+
+    for line in stdin().lines() {
+        let line = line?;
+
+        let numbers = line
+            .split(' ')
+            .map(|s| s.parse::<i64>())
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let mut differentiated = numbers.clone();
+        let mut extrapolated = *numbers.last().unwrap();
+
+        loop {
+            differentiated = differentiate(&differentiated);
+            if differentiated.iter().all(|n| *n == 0) {
+                break;
+            }
+            extrapolated += *differentiated.last().unwrap();
+        }
+
+        total += extrapolated;
+    }
+
+    println!("{}", total);
+
+    Ok(())
 }
