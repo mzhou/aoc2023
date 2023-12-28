37da8e047a8b24a9ea4e15bbdbc5787d59fba74d
Michael Zhou <mzhou@cse.unsw.edu.au>
2023-12-28 22:34:57 +0900

    day 18 resume

--- aoc2023_18/src/main.rs
+++ aoc2023_18/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+
+use anyhow::{Context, Error};
+
+struct Instruction {
+    color: u32,
+    direction: u8,
+    steps: u8,
+}
+
+fn main() -> Result<(), Error> {
+    let initial_x = 0usize;
+    let initial_y = 0usize;
+
+    for line in stdin().lines() {
+        let line = line?;
+        let line_bytes = line.as_bytes();
+
+        let mut parts = line_bytes.split(|b| *b == b' ');
+        let direction = parts.next().context("missing direction")?;
+    }
+
+    Ok(())
 }
